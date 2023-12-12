4982a9ef2050b759680657e873c04087aaf59559
calum <calum@quinns.ch>
2023-12-12 15:25:40 +0100

    Answer and check first 2

--- Requetes.sql
+++ Requetes.sql
@@ -1,137 +1,58 @@
--- Point 1
-SELECT DISTINCT C.*
-FROM Client C
-JOIN Réservation R ON C.id = R.idClient
-JOIN Hôtel H ON R.idChambre = H.id
-WHERE C.idVille = H.idVille;
+-- Point 1 Les clients ayant fait au moins une réservation dans un hôtel se trouvant dans la ville dans laquelle ils habitent.
+SELECT DISTINCT Client.nom,Client.prénom FROM Client 
+JOIN Réservation ON Client.id = Réservation.idClient
+JOIN Hôtel ON Réservation.idChambre = Hôtel.id
+where Client.idVille = Hôtel.idVille;
 
--- Point 2
-SELECT MIN(C.prixParNuit) AS prixMin, MAX(C.prixParNuit) AS prixMax, AVG(C.prixParNuit) AS prixAvg
-FROM Chambre C
-JOIN Hôtel H ON C.idHôtel = H.id
-JOIN Ville V ON H.idVille = V.id
-WHERE V.nom = 'Montreux';
+-- Point 2 Le prix minimum, maximum et moyen pour passer une nuit dans une chambre d'hôtel dans la ville de Montreux.
+SELECT min(prixParNuit) AS "Minimum", max(prixParNuit) AS "Maximum", avg(prixParNuit) AS "Moyen" FROM chambre
+JOIN Hôtel ON Chambre.idhôtel = Hôtel.id
+JOIN Ville ON Ville.id = Hôtel.idville
+WHERE Ville.nom = 'Montreux';
 
 
--- Point 3
-SELECT DISTINCT C.*
-FROM Client C
-JOIN Réservation R ON C.id = R.idClient
-JOIN Hôtel H ON R.idChambre = H.id
-WHERE H.nbEtoiles <= 2
-  AND NOT EXISTS (
-    SELECT 1
-    FROM Réservation R2
-    JOIN Hôtel H2 ON R2.idChambre = H2.id
-    WHERE C.id = R2.idClient AND H2.nbEtoiles > 2
-  );
+-- Point 3 Les clients qui n'ont fait des réservations que dans des hôtels de 2 étoiles ou moins.
 
 
--- Point 4
-SELECT DISTINCT V.nom
-FROM Ville V
-JOIN Hôtel H ON V.id = H.idVille
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM Réservation R
-    JOIN Chambre C ON R.idChambre = C.idHôtel
-    WHERE H.id = C.idHôtel
-);
+
+-- Point 4 Le nom des villes avec au moins un hôtel qui n'a aucune réservation.
 
 
--- Point 5
-SELECT H.id, H.nom, COUNT(DISTINCT C.numéro) AS nbTarifs
-FROM Hôtel H
-JOIN Chambre C ON H.id = C.idHôtel
-GROUP BY H.id, H.nom
-ORDER BY nbTarifs DESC
-LIMIT 1;
+
+-- Point 5 L'hôtel qui a le plus de tarifs de chambres différents.
 
 
--- Point 6
-SELECT R.idClient, R.idChambre, R.numéroChambre, COUNT(*) AS nbRéservations
-FROM Réservation R
-GROUP BY R.idClient, R.idChambre, R.numéroChambre
-HAVING COUNT(*) > 1;
+
+-- Point 6 Les clients ayant réservé plus d'une fois la même chambre. Indiquer les clients et les chambres concernées.
 
 
--- Point 7
-SELECT M.idClient, M.idHôtel
-FROM Membre M
-JOIN Hôtel H ON M.idHôtel = H.id
-WHERE H.nom = 'Kurz Alpinhotel'
-  AND NOT EXISTS (
-    SELECT 1
-    FROM Réservation R
-    WHERE M.idClient = R.idClient AND M.idHôtel = R.idChambre
-);
+
+-- Point 7 Les membres de l'hôtel "Kurz Alpinhotel" qui n'y ont fait aucune réservation depuis qu'ils en sont devenus membre.
 
 
--- Point 8
-SELECT V.nom, SUM(L.nbPlaces) AS capacitéTotale
-FROM Ville V
-JOIN Hôtel H ON V.id = H.idVille
-JOIN Chambre C ON H.id = C.idHôtel
-JOIN Lit L ON C.idHôtel = L.idEquipement
-GROUP BY V.nom
-ORDER BY capacitéTotale DESC;
+
+-- Point 8 Les villes, classées dans l'ordre décroissant de leur capacité d'accueil totale (nombre de places des lits de leurs hôtels).
 
 
--- Point 9
-SELECT H.id, H.nom, H.nbEtoiles, V.nom AS ville, COUNT(R.idClient) AS nbRéservations
-FROM Hôtel H
-JOIN Ville V ON H.idVille = V.id
-LEFT JOIN Chambre C ON H.id = C.idHôtel
-LEFT JOIN Réservation R ON C.idHôtel = R.idChambre
-GROUP BY H.id, H.nom, H.nbEtoiles, V.nom
-ORDER BY V.nom, nbRéservations DESC;
+
+-- Point 9 Les hôtels avec leur classement par ville en fonction du nombre de réservations.
 
 
--- Point 10
-SELECT R.*, CASE WHEN H.rabaisMembre IS NOT NULL THEN 'Oui' ELSE 'Non' END AS aObtenuRabais
-FROM Réservation R
-JOIN Chambre C ON R.idChambre = C.idHôtel
-JOIN Hôtel H ON C.idHôtel = H.id
-WHERE H.nom = 'Antique Boutique Hôtel'
-  AND R.dateArrivée >= CURRENT_DATE
-ORDER BY R.dateArrivée;
+
+-- Point 10 Lister, par ordre d'arrivée, les prochaines réservations pour l'hôtel "Antique Boutique Hôtel" en indiquant si le client a obtenu un rabais.
 
 
--- Point 11
-SELECT R.*
-FROM Réservation R
-JOIN Chambre C ON R.idChambre = C.idHôtel
-JOIN Lit L ON C.idHôtel = L.idEquipement
-WHERE R.nbPersonnes < L.nbPlaces;
+
+-- Point 11 Les réservations faites dans des chambres qui ont un nombre de lits supérieur au nombre de personnes de la réservation.
 
 
--- Point 12
-SELECT C.*
-FROM Chambre C
-JOIN Hôtel H ON C.idHôtel = H.id
-JOIN Ville V ON H.idVille = V.id
-JOIN Chambre_Equipement CE ON C.idHôtel = CE.idChambre
-JOIN Equipement E ON CE.idEquipement = E.id
-WHERE V.nom = 'Lausanne' AND E.nom = 'TV' AND EXISTS (
-    SELECT 1
-    FROM Chambre_Equipement CE2
-    JOIN Equipement E2 ON CE2.idEquipement = E2.id
-    WHERE C.idHôtel = CE2.idChambre AND E2.nom = 'Lit à 2 places'
-);
+
+-- Point 12 Les chambres à Lausanne ayant au moins une TV et un lit à 2 places.
 
 
--- Point 13
-SELECT R.*, (R.dateArrivée - R.dateRéservation) AS joursAvance, 
-    CASE WHEN M.idClient IS NOT NULL THEN 'Oui' ELSE 'Non' END AS estMembre
-FROM Réservation R
-JOIN Chambre C ON R.idChambre = C.idHôtel
-LEFT JOIN Membre M ON R.idClient = M.idClient AND C.idHôtel = M.idHôtel
-WHERE H.nom = 'Hôtel Royal'
-ORDER BY joursAvance DESC, R.nom, R.prénom;
+
+-- Point 13 Pour l'hôtel "Hôtel Royal", lister toutes les réservations en indiquant de combien de jours elles ont été faites à l'avance (avant la date d'arrivée) ainsi que si la réservation a été faite en tant que membre de l'hôtel. Trier les résultats par ordre des réservations (en 1er celles faites le plus à l’avance), puis par clients (ordre croissant du nom puis du prénom).
 
 
--- Point 14
-SELECT SUM(C.prixParNuit * R.nbNuits) AS prixTotal
-FROM Réservation R
-JOIN Chambre C ON R.idChambre = C.idHôtel
-WHERE C.idHôtel = (SELECT id FROM Hôtel WHERE nom = 'Hôtel Royal');
+
+-- Point 14 Calculer le prix total de toutes les réservations faites pour l'hôtel "Hôtel Royal".
